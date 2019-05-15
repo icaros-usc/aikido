1d94195dc997966c6e22de58f3e5d73984929007
Stefanos19 <snikolaidis1983@gmail.com>
2019-05-15 14:35:41 -0700

    fixed ompl dependencies

--- .ci/install_linux.sh
+++ .ci/install_linux.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
+# Install OMPL package from PRL PPA
+if [ $(lsb_release -sc) = "xenial" ]; then
+  sudo apt-add-repository -y ppa:personalrobotics/ppa
+  sudo apt update
+  sudo apt install -y libompl-dev # OMPL (>= 1.2.1)
+fi
+
+
 cd "${HOME}/workspace"
 cp -r "${TRAVIS_BUILD_DIR}" src
 ./scripts/internal-distro.py --workspace=src distribution.yml --repository "${REPOSITORY}" ${REQUIRED_ONLY}
